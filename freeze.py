da2e0cea1c723fcb1ea7c7506c2103a5e3dfa1b8
Kyle Dela Pena <kczendel@github.com>
2026-04-18 16:40:14 -0500

    Moving static files to root for deployment

--- freeze.py
+++ freeze.py
@@ -2,12 +2,15 @@ from flask_frozen import Freezer
 from app import app
 import mimetypes
 
+# Fix for Windows MIME type identification
 mimetypes.add_type('text/css', '.css')
 mimetypes.add_type('application/javascript', '.js')
+
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-    print("Freezing the site...")
+    print("Freezing the site for GitHub Pages...")
     freezer.freeze()
-    print("Done!")
+    print("Done! Your static site is ready in the 'build' folder.")
